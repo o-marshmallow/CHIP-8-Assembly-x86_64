c595d39796760abc7149c6efecf1062ab440c813
Omar <elachich@informatique.univ-paris-diderot.fr>
2017-06-11 20:38:46 +0200

    Added if-then-else structure for F-opcodes

--- cpu_f_opcode.s
+++ cpu_f_opcode.s
@@ -5,5 +5,35 @@
         .include "macros.s"
         .text
 
+        ## Opcode is in register rax
 opcode_f:
-        ret
+        mov %r8, %rax
+        cmp %r8b, 0x07
+        jne op_0a
+        ## LD Vx, DT
+op_0a:  cmp %r8b, 0x0A
+        jne op_15
+        ## LD Vx, K
+op_15:  cmp %r8b, 0x15
+        jne op_18
+        ## LD DT, Vx
+op_18:  cmp %r8b, 0x18
+        jne op_1e
+        ## LD ST, Vx
+op_1e:  cmp %r8b, 0x1e
+        jne op_29
+        ## ADD I, Vx
+op_29:  cmp %r8b, 0x29
+        jne op_33
+        ## LD F, Vx
+op_33:  cmp %r8b, 0x33
+        jne op_55
+        ## LD B, Vx
+op_55:  cmp %r8b, 0x55
+        jne op_65
+        ## LD [I], Vx
+op_65:  cmp %r8b, 0x65
+        jne unknown_opcode
+        ## LD Vx, [I]
+
+return: ret
